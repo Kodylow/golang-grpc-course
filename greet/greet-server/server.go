1b9dbc2d0908eb4672e3457b4ca10fffbb99ca7a
Kody Low <kodylow7@gmail.com>
2021-10-28 13:59:08 -0700

    boilerplate

---
+++ greet/greet-server/server.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"github.com/kodylow/golang-grpc-course/greet/greetpb"
+	"google.golang.org/grpc"
+)
+
+type server struct{}
+
+func main() {
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		log.Fatal("Failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	greetpb.RegisterGreetServiceServer(s, &server{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatal("Failed to listen: %v", err)
+	}
+}
